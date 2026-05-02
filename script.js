ee055c4ffabfbfceb709996fca2ab0be89b50605
이루리 <nala7979@naver.com>
2026-05-03 00:07:54 +0900

    UI 삭제

--- script.js
+++ script.js
@@ -340,23 +340,3 @@ const facilityColorObserver = new IntersectionObserver((entries)=>{
 facilityColorItems.forEach(item=>{
   facilityColorObserver.observe(item);
 });
-
-/* 탭 기능 */
-document.querySelectorAll(".tab-btn").forEach(btn=>{
-  btn.addEventListener("click", function(){
-
-    const target = this.dataset.tab;
-
-    document.querySelectorAll(".tab-btn").forEach(b=>{
-      b.classList.remove("active");
-    });
-
-    document.querySelectorAll(".tab-content").forEach(c=>{
-      c.classList.remove("active");
-    });
-
-    this.classList.add("active");
-    document.getElementById(target).classList.add("active");
-
-  });
-});

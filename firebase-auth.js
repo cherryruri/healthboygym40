c0ecb7b662f607141bdf29385daf59a0d15de018
cherryruri <nala7979@naver.com>
2026-05-24 04:09:43 +0900

    .

--- firebase-auth.js
+++ firebase-auth.js
@@ -89,3 +89,52 @@ window.signup = async function(){
   }
 
 }
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6fYLWkH9oSr7f-H4QNHUuN7Y2bFOvgQ8",
+  authDomain: "healthboygym40-4ee44.firebaseapp.com",
+  projectId: "healthboygym40-4ee44",
+  storageBucket: "healthboygym40-4ee44.firebasestorage.app",
+  messagingSenderId: "924150165105",
+  appId: "1:924150165105:web:860ff60aaafb61b722b5d0",
+  measurementId: "G-4CJK3XF633"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user)=>{
+
+  const loginLink = document.getElementById("miniLogin");
+  const logoutBtn = document.getElementById("miniLogout");
+
+  if(!loginLink || !logoutBtn) return;
+
+  if(user){
+    const name = user.email.split("@")[0];
+
+    loginLink.textContent = `${name}님 환영합니다`;
+    loginLink.href = "#";
+
+    logoutBtn.style.display = "inline-block";
+  }else{
+    loginLink.textContent = "LOGIN";
+    loginLink.href = "login.html";
+
+    logoutBtn.style.display = "none";
+  }
+
+});
+
+window.logoutUser = async function(){
+  await signOut(auth);
+  location.reload();
+};

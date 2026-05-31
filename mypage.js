d9d8287f38e8949e15a546d93f890c61ed2fe35e
cherryruri <nala7979@naver.com>
2026-06-01 02:00:56 +0900

    ㅇ

--- mypage.js
+++ mypage.js
@@ -43,10 +43,11 @@ function setText(el, value){
 onAuthStateChanged(auth, async (user)=>{
 
 if(!user){
-  alert("로그인 후 이용할 수 있습니다.");
   location.href = "login.html";
   return;
 }
+
+
   const userId = user.email ? user.email.split("@")[0] : "회원";
 
   setText(myName, `${userId}님`);

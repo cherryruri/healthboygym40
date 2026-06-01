e15a9206f76250d0544d87adb32028d3e05306fb
cherryruri <nala7979@naver.com>
2026-06-02 07:44:52 +0900

    d

--- post.js
+++ post.js
@@ -121,9 +121,14 @@ async function loadComments() {
         <div class="comment-right">
 
           <div class="comment-meta">
+
+            <span class="writer">${c.writer}</span>
+  <span class="time">${timeAgo(c.createdAt)}</span>
+
           <button class="like-btn" data-id="${docSnap.id}">
-  ❤️ ${c.likes || 0}
-</button>
+           ❤️ ${c.likes || 0}
+          </button>
+          
             <span class="writer">${c.writer}</span>
             <span class="time">${timeAgo(c.createdAt)}</span>
           </div>

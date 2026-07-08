49fa6b2e19b72f6ca31fbf9f1d4ab881e16423f9
cherryruri <nala7979@naver.com>
2026-07-09 01:46:24 +0900

    Make mobile reviews scroll continuously

--- home-mobile.js
+++ home-mobile.js
@@ -47,4 +47,79 @@
       counterForceFinishProgress:0.54
     }
   };
+
+  function installContinuousReviewSlider(){
+    if(window.HEALTHBOY_REVIEW_SWIPER_PATCHED) return;
+    window.HEALTHBOY_REVIEW_SWIPER_PATCHED = true;
+
+    function isReviewTarget(target){
+      if(typeof target === "string") return target.indexOf("all_slider") !== -1;
+      return !!(
+        target &&
+        target.classList &&
+        target.classList.contains("all_slider")
+      );
+    }
+
+    function tuneOptions(target, options){
+      if(!isReviewTarget(target) || !options) return options;
+
+      options.speed = 6500;
+      options.centeredSlides = false;
+      options.loopAdditionalSlides = Math.max(options.loopAdditionalSlides || 0, 6);
+      options.autoplay = Object.assign({}, options.autoplay || {}, {
+        delay:0,
+        disableOnInteraction:false,
+        pauseOnMouseEnter:false
+      });
+      options.freeMode = true;
+      options.freeModeMomentum = false;
+
+      return options;
+    }
+
+    function wrapSwiper(SwiperCtor){
+      if(!SwiperCtor || SwiperCtor.__healthboyContinuousReview) return SwiperCtor;
+
+      function ReviewSwiper(target, options){
+        return new SwiperCtor(target, tuneOptions(target, options));
+      }
+
+      Object.keys(SwiperCtor).forEach(function(key){
+        try{
+          ReviewSwiper[key] = SwiperCtor[key];
+        }catch(error){}
+      });
+
+      ReviewSwiper.prototype = SwiperCtor.prototype;
+      ReviewSwiper.__healthboyContinuousReview = true;
+      return ReviewSwiper;
+    }
+
+    var descriptor = Object.getOwnPropertyDescriptor(window, "Swiper");
+    var currentSwiper = window.Swiper;
+
+    if(descriptor && descriptor.configurable === false){
+      if(currentSwiper) window.Swiper = wrapSwiper(currentSwiper);
+      return;
+    }
+
+    try{
+      Object.defineProperty(window, "Swiper", {
+        configurable:true,
+        get:function(){
+          return currentSwiper;
+        },
+        set:function(value){
+          currentSwiper = wrapSwiper(value);
+        }
+      });
+
+      if(currentSwiper) currentSwiper = wrapSwiper(currentSwiper);
+    }catch(error){
+      if(currentSwiper) window.Swiper = wrapSwiper(currentSwiper);
+    }
+  }
+
+  installContinuousReviewSlider();
 })();
